529217ec3ac0ae5bb6dd79c6db34c81553a1b910
Samuel Müller <samuel.mueller@tuta.io>
2025-04-25 00:00:45 +0200

    update run

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 MOUNT_SRC=${1:-$PWD}          # ${var:-default}
+DISP=$( [[ "$(uname)" == "Linux" ]] && ${DISPLAY} || echo "host.docker.internal:0" )
 
 echo "Mounting host path: $MOUNT_SRC -> /work"
 
 docker run -it --rm \
-  -e DISPLAY=host.docker.internal:0 \
+  -e DISPLAY=$DISP \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v "$MOUNT_SRC":/work -w /work \
   ghcr.io/jumpingsloth/ghdl-gtkwave
